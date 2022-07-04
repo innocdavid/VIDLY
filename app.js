33e5127c0ffc8fcd5d927e51fdf5eaca1d752d4a
Banja David Lotigo <innocdavid@gmail.com>
2022-07-04 20:27:44 +0300

    API CRUD OPERATIONS - adding a get route to list all the genres - adding a post route to add new genre to the array - adding a get to list a single genre - adding a put route to update a single genre - adding a delete route to remove a single genre

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 // MIDDLEWARE
 app.use(express.json());
 
-// setting routes
+// setting up routes
 const genRoute = require('./routes/genreRouter');
 
 // using routes
@@ -16,6 +16,7 @@ app.use('/', genRoute);
 // data
 const port = process.env.PORT || 3000;
 const hostname = process.env.HOSTNAME;
+
 // server
 app.listen(port, hostname, () => {
   if (process.env.NODE_ENV !== 'test') console.log(`server running at ${hostname}://${port}`);
--- routes/genreRouter.js
+++ routes/genreRouter.js
@@ -1,11 +1,82 @@
 // modules
 const express = require('express');
+const Joi = require('joi');
 const router = express.Router();
+
+// creating genres
+const genres = [
+  {id: 1, name: 'Action'},
+  {id: 2, name: 'Romance'},
+  {id: 3, name: 'Sci-Fi'},
+  {id: 4, name: 'Horror'},
+  {id: 5, name: 'Fantasy'},
+  {id: 6, name: 'Crime'},
+  {id: 7, name: 'Sport'},
+  {id: 8, name: 'Animated'},
+];
 
 //setting up routes
 router.get('/', (req, res) => {
   res.send('Welcome, Movie Homepage');
 });
+
+// list all genres
+router.get('/api/genres', (req, res) => {
+  res.send(genres)
+});
+
+// posting a new genre
+router.post('/api/genres', (req, res) => {
+  // validation
+  const { error } = validateGenres(req.body);
+  if (error) return res.status(404).send(error.details[0].message);
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name
+  }
+
+  genres.push(genre);
+  res.send(genre);
+});
+
+// getting a single genre
+router.get('/api/genres/:id', (req, res) => {
+  const genre = genres.find(g => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send(`genre with given id ${req.params.id} not found`);
+  res.send(genre);
+});
+
+// update a genre
+router.put('/api/genres/:id', (req, res) => {
+  const genre = genres.find(g => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send(`genre with given id ${req.params.id} not found`);  
+
+  const { error } = validateGenres(req.body);
+  if (error) return res.status(404).send(error.details[0].message);
+
+  genre.name = req.body.name;
+  res.send(genre);
+});
+
+// delete a genre
+router.delete('/api/genres/:id', (req, res) => {
+  const genre = genres.find(g => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send(`genre with given id ${req.params.id} not found`);
+
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+  
+  res.send(genre);
+});
+
+// schema validation
+function validateGenres(genre) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required()
+  });
+  return schema.validate(genre);
+}
 
 // exporting routes
 module.exports = router;
